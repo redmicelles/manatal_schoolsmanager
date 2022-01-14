068b744254e5bf963dc3809c4702f08d8156dcad
Oluwaseyi Daniel <dexter_daniel@outlook.com>
2022-01-14 18:30:28 +0100

    finished step1

--- core/settings.py
+++ core/settings.py
@@ -12,16 +12,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env files in env_files directory
+for file in os.listdir(os.path.join(BASE_DIR, 'env_files')):
+    environ.Env.read_env(os.path.join(BASE_DIR, 'env_files/.env.pg_db'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,22 +87,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': 'pg_db',
+        'PORT': 5432,
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_NAME'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': 'pg_db',
-#         'PORT': 5432,
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -132,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- schoolsmanager/models.py
+++ schoolsmanager/models.py
@@ -1,3 +1,40 @@
+from tkinter import CASCADE
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class School(models.Model):
+
+    """"""
+    schoolname = models.CharField(max_length=20, null=False, verbose_name="School Name")
+    maxstudents = models.IntegerField(validators = [MinValueValidator(0)], verbose_name="Maximum Number of Students Allowed")
+
+    class Meta:
+
+        """"""
+        ordering = ["schoolname"]
+        verbose_name = "Schools Schema"
+
+    def __str__(self):
+
+        """"""
+        return self.schoolname
+
+class Student(models.Model):
+
+    """"""
+    firstname = models.CharField(max_length=20, null=False, verbose_name="First Name")
+    lastname = models.CharField(max_length=20, verbose_name="Last Name")
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+    class Meta:
+
+        """"""
+        ordering = ["firstname"]
+        verbose_name = "Students Schema"
+
+    def __str__(self):
+
+        """"""
+        return f"{self.firstname} {self.lastname}"
