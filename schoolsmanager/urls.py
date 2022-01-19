a32472d4b7e681af34402cb40ac579c62ed0ad0d
Oluwaseyi Daniel <dexter_daniel@outlook.com>
2022-01-19 04:11:59 +0100

    started step3

--- schoolsmanager/urls.py
+++ schoolsmanager/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
-from .views import index
+from rest_framework_nested import routers
 from rest_framework.routers import DefaultRouter
-from .views import SchoolViewSet, StudentViewSet,index
+from .views import SchoolViewSet, StudentViewSet
 
 router = DefaultRouter()
 router.register('schools', SchoolViewSet, basename=None)
 router.register('students', StudentViewSet, basename=None)
 
 urlpatterns = [
-    path('', index),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
